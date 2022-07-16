868f350ab302c45ccd89a1a4405e445a15b06b6f
saamon <suzuken2960@gmail.com>
2022-07-17 01:20:33 +0900

    Next.js

---
+++ pages/products/index.js
@@ -0,0 +1,33 @@
+import styles from "../../styles/Home.module.css"
+import Link from "next/link";
+const ProductsList = () => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h2 className={styles.title}>商品一覧</h2>
+
+                <ul>
+                    <li>
+                        <Link href="products/smartphone">
+                        <a>スマートフォン</a>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="products/pc">
+                            <a>パソコン</a>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link href="products/headphone">
+                            <a>ヘッドフォン</a>
+                        </Link>
+                    </li>
+                </ul>
+            </main>
+        </div>
+    );
+};
+
+export default ProductsList;
